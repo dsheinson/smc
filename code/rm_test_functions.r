02f95422716edff74d3bb2279af5d6f18e3a5cf7
dsheinson <sheinson@pstat.ucsb.edu>
2013-10-22 17:14:18 -0700

    Updated sample.states function in rm_pf to decrease computing time

--- code/rm_test_functions.r
+++ code/rm_test_functions.r
@@ -13,7 +13,6 @@ rprior <- function(j,a,b)
   return(list(x=mystate,theta=mytheta))
 }
 
-require(dlm)
 rm_mcmc <- function(y, x, theta, a0, b0, n.iter)
 {
   for(t in 1:n.iter)
@@ -22,8 +21,7 @@ rm_mcmc <- function(y, x, theta, a0, b0, n.iter)
     theta = sample.theta(y, x, theta, a0, b0)
 
     # Sample states by FFBS
-    mydlm = dlm(list(m0=0, C0=theta, FF=1, V=theta, GG=1, W=theta))
-    x = dlmBSample(dlmFilter(y, mydlm))
+    x = sample.states(y, theta)
   }
   return(list(state=x,theta=theta))
 }
@@ -38,6 +36,43 @@ sample.theta <- function(y, x, theta, a0, b0)
   a = K + 1.5 + a0 - 1
   b = .5*(sum((y - x[2:(K+1)])^2) + sum((x[2:(K+1)] - x[1:K])^2) + x[1]^2 + 2*b0)
   return(1/rgamma(1,a,b))
+}
+
+sample.states <- function(y, theta)
+{
+  # Initialize DLM
+  nt = length(y)
+  W = V = theta
+  F = G = 1
+
+  # Forward-filtering
+  m = rep(NA, nt + 1)
+  C = rep(NA, nt + 1)
+  f = rep(NA, nt)
+  Q = rep(NA, nt)
+  A = rep(NA, nt)
+  R = rep(NA, nt)
+  m[1] = 0; C[1] = theta
+  for(i in 1:nt)
+  {
+    A[i] = G*m[i]; R[i] = G*C[i]*G + W
+    f[i] = F*A[i]; Q[i] = F*R[i]*F + V  
+    e = y[i] - f[i]; Qinv = 1 / Q[i]
+    RFQinv = R[i]*F*Qinv
+    m[i+1] = A[i] + RFQinv*e
+    C[i+1] = R[i] - RFQinv*F*R[i]
+  }
+
+  # Backward-sampling
+  x = rep(NA, nt + 1)
+  x[nt + 1] = rnorm(1, m[nt + 1], sqrt(C[nt + 1]))
+  for(i in nt:1)
+  {
+    h = m[i] + C[i]*G*(1/R[i])*(x[i+1] - A[i])
+    H = C[i] - C[i]*G*(1/R[i])*G*C[i]
+    x[i] = rnorm(1, h, sqrt(H))
+  }
+  return(x)
 }
 
 sample.theta.mh <- function(y, x, theta)

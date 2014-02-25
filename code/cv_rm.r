de62a07eb846fe5e34d45e8e9329e8b8f23a406c
dsheinson <sheinson@pstat.ucsb.edu>
2014-02-25 10:00:48 -0800

    New runs without setting seed

--- code/cv_rm.r
+++ code/cv_rm.r
@@ -10,7 +10,8 @@ dpath = "../data/"
 # Load simulated data
 load(paste(dpath,"rw_sim.rdata",sep=""))
 
-pf_lmarglik <- function(n.sim, np, W, seed, alpha = 0.05, progress = FALSE)
+# Function to run pfs
+pf_lmarglik <- function(n.sim, np, W, label, alpha = 0.05, progress = FALSE)
 {
   a0 = b0 = 1
   mydlm = list(F = 1, G = 1, V = 1, W = W, m0 = 0, C0 = 1)
@@ -18,7 +19,7 @@ pf_lmarglik <- function(n.sim, np, W, seed, alpha = 0.05, progress = FALSE)
   revo1 = function(x, theta) revo(x, sigma2 = theta)
   rprior1 <- function() rprior(a0,b0)
   rmove <- function(y, x, theta) rm_mcmc(y, x, theta, a0, b0, mydlm, 1)
-  set.seed(seed)
+  rnorm(1)
   out = rm_pf(mysims[[n.sim]]$y, dllik1, revo1, rprior1, rmove, np, progress=progress, method="stratified", nonuniformity="ess", threshold=0.8, log=FALSE)
   
   # Calculate log-marginal likelihood
@@ -30,16 +31,17 @@ pf_lmarglik <- function(n.sim, np, W, seed, alpha = 0.05, progress = FALSE)
   
   pf.out = list(lmarglik=lmarglik, state.quant=state.quant, theta.quant=theta.quant)
   cat(n.sim,np,W,seed,'\n')
-  save(pf.out, file=paste(dpath,"cv_pf-",n.sim,"-",np,"-",W,"-",seed,"-",alpha,".rdata",sep=""))
+  save(pf.out, file=paste(dpath,"cv_pf-",n.sim,"-",np,"-",W,"-",label,"-",alpha,".rdata",sep=""))
 }
 
 # Apply pf_lmarglik for many pf runs with different number of particles and values of W
-data1 = expand.grid(n.sim=rep(3,20),np=c(100, 500, 1000, 5000), W=c(0.5,1,2), stringsAsFactors=FALSE)
-data1 = data.frame(data1, seed = 1:dim(data1)[1])
-data2 = expand.grid(n.sim=c(1,2,4:20),np=c(100, 500, 1000, 5000), W=c(0.5,1,2), stringsAsFactors=FALSE)
-data2 = data.frame(data2, seed = (dim(data1)[1] + 1:dim(data2)[1]))
+data1 = expand.grid(n.sim=rep(c(1,3),c(20,20)),np=c(100, 500, 1000, 5000), W=c(0.5,1,2), stringsAsFactors=FALSE)
+data1 = data.frame(data1, label = 1:dim(data1)[1])
+data2 = expand.grid(n.sim=c(2,4:20),np=c(100, 500, 1000, 5000), W=c(0.5,1,2), stringsAsFactors=FALSE)
+data2 = data.frame(data2, label = (dim(data1)[1] + 1:dim(data2)[1]))
 mydata = rbind(data1,data2)
 require(doMC)
 registerDoMC()
 require(plyr)
+set.seed(61)
 m_ply(.data = mydata, .fun = pf_lmarglik, .parallel = TRUE)

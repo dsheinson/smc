8ce46c8c367e8bd7075c010371968ba2a0340025
dsheinson <sheinson@pstat.ucsb.edu>
2014-02-25 23:39:51 -0800

    Updated code to construct plots for cv DLM model

--- code/cv_pf_plots.r
+++ code/cv_pf_plots.r
@@ -12,22 +12,22 @@ load(paste(dpath,"rw_sim.rdata",sep=""))
 N = length(mysims)
 
 # Create data set of names of rm pf runs
-np = c(100, 500, 1000, 5000)
-nsims = 20
-cols = c(3,4,6,7)
-data1 = expand.grid(n.sim=rep(1,20),np=np, W=c(0.5,1,2), stringsAsFactors=FALSE)
-data1 = data.frame(data1, seed = 1:dim(data1)[1])
-data2 = expand.grid(n.sim=2:20,np=np, W=c(0.5,1,2), stringsAsFactors=FALSE)
-data2 = data.frame(data2, seed = (dim(data1)[1] + 1:dim(data2)[1]))
+data1 = expand.grid(n.sim=rep(1,20),np=c(100,500,1000,5000), W=c(0.5,1,2), stringsAsFactors=FALSE)
+data1 = data.frame(data1, label = 1:dim(data1)[1])
+data2 = expand.grid(n.sim=2:20,np=c(100,500,1000,5000), W=c(0.5,1,2), stringsAsFactors=FALSE)
+data2 = data.frame(data2, label = (dim(data1)[1] + 1:dim(data2)[1]))
 mydata = rbind(data1,data2)
+np = unique(mydata$np)
+W = unique(mydata$W)
+nsims = unique(mydata$n.sim)
+cols = rainbow(length(np))
 
 ## Function to plot quantiles from rm_pf, compared against true posterior
-cv_pf_quantiles <- function(n.sim, W = 1, alpha = 0.05, burn.k = 1, burn.p = 5)
+cv_pf_quantiles <- function(n.sim, W, alpha = 0.05, burn.k = 1, burn.p = 5)
 {
   F = mysims[[n.sim]]$true.params$F[1,1,1]
   G = mysims[[n.sim]]$true.params$G[1,1]
   V = mysims[[n.sim]]$true.params$V[1,1]
-  W = mysims[[n.sim]]$true.params$W[1,1]
   m0 = mysims[[n.sim]]$true.params$m0
   C0 = mysims[[n.sim]]$true.params$C0[1,1]
   nt = dim(mysims[[n.sim]]$y)[2]
@@ -46,10 +46,10 @@ cv_pf_quantiles <- function(n.sim, W = 1, alpha = 0.05, burn.k = 1, burn.p = 5)
   gmax.k = gmax.p = -Inf
   for(j in 1:length(np))
   {
-    seeds = mydata$seed[which(mydata$n.sim == n.sim & mydata$np == np[j] & mydata$W == W)]
-    for(i in 1:length(seeds))
+    labels = mydata$label[which(mydata$n.sim == n.sim & mydata$np == np[j] & mydata$W == W)]
+    for(i in 1:length(labels))
     {
-      load(paste(dpath,"cv_pf-",n.sim,"-",np[j],"-",W,"-",seeds[i],"-",alpha,".rdata",sep=""))
+      load(paste(dpath,"cv_pf-",n.sim,"-",np[j],"-",W,"-",labels[i],"-",alpha,".rdata",sep=""))
       gmin.k = min(gmin.k, lk[-(1:burn.k)], mysims[[n.sim]]$x, pf.out$state.quant[-(1:burn.k),1,1])
       gmax.k = max(gmax.k, uk[-(1:burn.k)], mysims[[n.sim]]$x, pf.out$state.quant[-(1:burn.k),1,2])
       gmin.p = min(gmin.p, lp[-(1:burn.p)], 1, pf.out$theta.quant[-(1:burn.p),1,1])
@@ -58,14 +58,14 @@ cv_pf_quantiles <- function(n.sim, W = 1, alpha = 0.05, burn.k = 1, burn.p = 5)
   }
   
   # Plot true data, states, 95% CI of filtered states, and 95% one-step ahead PI
-  pdf(paste(gpath,"cv-pf-states-",n.sim,".pdf",sep=""))
+  pdf(paste(gpath,"cv-pf-states-",n.sim,"-",W,"-",alpha,".pdf",sep=""))
   for(j in 1:length(np))
   {
-    seeds = mydata$seed[which(mydata$n.sim == n.sim & mydata$np == np[j] & mydata$W == W)]
-    for(i in 1:length(seeds))
+    labels = mydata$label[which(mydata$n.sim == n.sim & mydata$np == np[j] & mydata$W == W)]
+    for(i in 1:length(labels))
     {
-      load(paste(dpath,"cv_pf-",n.sim,"-",np[j],"-",W,"-",seeds[i],"-",alpha,".rdata",sep=""))
-      if(j == 1 & i == 1) plot(0:nt,mysims[[n.sim]]$x[1,],ylim=c(gmin.k,gmax.k),type="l",xlab=expression(t),ylab="Position")
+      load(paste(dpath,"cv_pf-",n.sim,"-",np[j],"-",W,"-",labels[i],"-",alpha,".rdata",sep=""))
+      if(j == 1 & i == 1) plot(0:nt,mysims[[n.sim]]$x[1,],ylim=c(gmin.k,gmax.k),type="l",xlab=expression(t),ylab=expression(x[t]))
       lines(0:nt, pf.out$state.quant[,1,1],col=cols[j])
       lines(0:nt, pf.out$state.quant[,1,2],col=cols[j])
     }
@@ -73,133 +73,143 @@ cv_pf_quantiles <- function(n.sim, W = 1, alpha = 0.05, burn.k = 1, burn.p = 5)
   lines(0:nt,lk,col=2,lwd=4)
   lines(0:nt,uk,col=2,lwd=4)
   title(expression(paste("Sequential 95% CI for ",sep=""),x[t]))
-  legend("bottomright", c(paste(np," particles",sep=""),"True post", "True sim"), lty=rep(1,length(np)+2), col = c(cols,2,1))
+  mtext(substitute(paste(tilde(W)," = ",aa),list(aa = W)),side=3)
+  legend("bottomright", c(paste(np," particles",sep=""),"True post", "True sim"), lwd=c(rep(1,length(np)),4,1), lty=rep(1,length(np)+2), col = c(cols,2,1))
   dev.off()
   
   # Plot 95% CI of filtered precision
-  pdf(paste(gpath,"cv-pf-precision-",n.sim,".pdf",sep=""))
+  pdf(paste(gpath,"cv-pf-precision-",n.sim,"-",W,"-",alpha,".pdf",sep=""))
   for(j in 1:length(np))
   {
-    seeds = mydata$seed[which(mydata$n.sim == n.sim & mydata$np == np[j] & mydata$W == W)]
-    for(i in 1:length(seeds))
+    labels = mydata$label[which(mydata$n.sim == n.sim & mydata$np == np[j] & mydata$W == W)]
+    for(i in 1:length(labels))
     {
-      load(paste(dpath,"cv_pf-",n.sim,"-",np[j],"-",W,"-",seeds[i],"-",alpha,".rdata",sep=""))
-      if(j == 1 & i == 1) plot(0:nt,rep(1,nt+1),ylim=c(gmin.p,gmax.p),type="l",xlab=expression(t),ylab=expression(sigma^2))
+      load(paste(dpath,"cv_pf-",n.sim,"-",np[j],"-",W,"-",labels[i],"-",alpha,".rdata",sep=""))
+      if(j == 1 & i == 1) plot(0:nt,rep(1,nt+1),ylim=c(gmin.p,gmax.p),type="l",xlab=expression(t),ylab=expression(phi))
       lines(0:nt, pf.out$theta.quant[,1,1],col=cols[j])
       lines(0:nt, pf.out$theta.quant[,1,2],col=cols[j])
     }
   }
   lines(0:nt,lp,col=2,lwd=4)
   lines(0:nt,up,col=2,lwd=4)
-  title(expression(paste("Sequential 95% CI for ",sep=""),sigma^2))
-  legend("topright", c(paste(np," particles",sep=""),"True post", "True sim"), lty=rep(1,length(np)+2), col = c(cols,2,1))
+  title(expression(paste("Sequential 95% CI for ",sep=""),phi))
+  mtext(substitute(paste(tilde(W)," = ",aa),list(aa = W)),side=3)
+  legend("topright", c(paste(np," particles",sep=""),"True post", "True sim"), lwd=c(rep(1,length(np)),4,1), lty=rep(1,length(np)+2), col = c(cols,2,1))
   dev.off()
 }
 
 require(plyr)
-m_ply(data.frame(n.sim=1:nsims), cv_pf_quantiles)
+m_ply(expand.grid(n.sim=nsims, W=W), cv_pf_quantiles)
 
 ## Plot kernel density estimates of log-likelihood under each model
-n.sim = 3
-alpha = 0.05
-W = unique(mydata$W)
-F = mysims[[n.sim]]$true.params$F[1,1,1]
-G = mysims[[n.sim]]$true.params$G[1,1]
-V = mysims[[n.sim]]$true.params$V[1,1]
-m0 = mysims[[n.sim]]$true.params$m0
-C0 = mysims[[n.sim]]$true.params$C0[1,1]
-nt = dim(mysims[[n.sim]]$y)[2]
-
-# Calculate true log marginal likelihoods under each model
-true_lmarglik = function(W)
+cv_pf_loglik <- function(n.sim, alpha = 0.05)
 {
-  post = cv.post(mysims[[n.sim]]$y, F, G, V, W, 1, 1, 0, 1)
-  return(cv.lmarglik(mysims[[n.sim]]$y, post$f, post$Q, post$a, post$b))
-}
-true.lmarglik = maply(data.frame(W=W), true_lmarglik)
+  #n.sim = 1
+  #alpha = 0.05
+  F = mysims[[n.sim]]$true.params$F[1,1,1]
+  G = mysims[[n.sim]]$true.params$G[1,1]
+  V = mysims[[n.sim]]$true.params$V[1,1]
+  m0 = mysims[[n.sim]]$true.params$m0
+  C0 = mysims[[n.sim]]$true.params$C0[1,1]
+  nt = dim(mysims[[n.sim]]$y)[2]
 
-# Load approximate log marginal likelihoods for particle filter runs
-rm.lmarglik = array(NA, c(nsims,length(np), length(W)))
-for(j in 1:length(np))
-{
-  for(k in 1:length(W))
+  # Calculate true log marginal likelihoods under each model
+  true_lmarglik = function(W)
   {
-    seeds = mydata$seed[which(mydata$n.sim == n.sim & mydata$np == np[j] & mydata$W == W[k])]
-    for(i in 1:length(seeds))
+    post = cv.post(mysims[[n.sim]]$y, F, G, V, W, 1, 1, 0, 1)
+    return(cv.lmarglik(mysims[[n.sim]]$y, post$f, post$Q, post$a, post$b))
+  }
+  true.lmarglik = maply(data.frame(W=W), true_lmarglik)
+
+  # Load approximate log marginal likelihoods for particle filter runs
+  rm.lmarglik = array(NA, c(length(nsims),length(np), length(W)))
+  for(j in 1:length(np))
+  {
+    for(k in 1:length(W))
     {
-      load(paste(dpath,"cv_pf-",n.sim,"-",np[j],"-",W[k],"-",seeds[i],"-",alpha,".rdata",sep=""))
-      rm.lmarglik[i,j,k] = pf.out$lmarglik
+      labels = mydata$label[which(mydata$n.sim == n.sim & mydata$np == np[j] & mydata$W == W[k])]
+      for(i in 1:length(labels))
+      {
+        load(paste(dpath,"cv_pf-",n.sim,"-",np[j],"-",W[k],"-",labels[i],"-",alpha,".rdata",sep=""))
+        rm.lmarglik[i,j,k] = pf.out$lmarglik
+      }
     }
   }
+
+  # Plot kernel density estimates of of log marginal likelihoods for pfs compared with truth
+  max.densities = aaply(rm.lmarglik, 2:3, function(a) max(density(a)$y,na.rm=TRUE))
+  min.densities = aaply(rm.lmarglik, 2:3, function(a) min(density(a)$y,na.rm=TRUE))
+  max.breaks = aaply(rm.lmarglik, 2:3, function(a) max(density(a)$x,na.rm=TRUE))
+  min.breaks = aaply(rm.lmarglik, 2:3, function(a) min(density(a)$x,na.rm=TRUE))
+  ylabs = c("Density","","")
+  xlabs = c("Log Marginal Likelihood","","")
+  cols = rainbow(length(np))
+  mains = c(substitute(paste(tilde(W)," = ",aa,sep=""),list(aa=W[1])),substitute(paste(tilde(W)," = ",aa,sep=""),list(aa=W[2])),substitute(paste(tilde(W)," = ",aa,sep=""),list(aa=W[3])))
+  pdf(file=paste(gpath,"cv_pf_loglik-",n.sim,".pdf",sep=""),width=15,height=5)
+  par(mfrow=c(1,3),mar=c(5,6,4,2)+0.1)
+  for(i in 1:length(W))
+  {
+    plot(density(rm.lmarglik[,1,i]),lwd=2,col=cols[1],main=mains[i],xlab=xlabs[i],ylab=ylabs[i],xlim=c(min(min.breaks),max(max.breaks)),ylim=c(min(min.densities[,i]),max(max.densities[,i])),cex.axis=1.5,cex.lab=1.75,cex.main=2)
+    if(length(np > 1)) for(j in 2:length(np)) lines(density(rm.lmarglik[,j,i]),lwd=2,col=cols[j])
+    abline(v=true.lmarglik[i],lwd=1,col=1)
+    for(j in which(!(1:length(W) %in% i))) abline(v=true.lmarglik[j],lwd=1,col=1,lty=2)
+    mtext(round(true.lmarglik[i],2),side=1,at=true.lmarglik[i],cex=0.75)
+    if(i == 1) legend("topleft",c(paste(np," particles",sep=""),"Truth","Truth (other models)"),lty=c(rep(1,length(np)),1,2),lwd=c(rep(2,length(np)),1,1),col=c(cols,1,1),cex=1.5)
+  }
+  dev.off()
+
+  # Compute true posterior model probabilities
+  true.postModProbs = postModProbs(true.lmarglik, rep(1/length(W), length(W)))
+  
+  # Compute rm_pf approximate posterior model probabilities
+  rm.postModProbs = aaply(rm.lmarglik, 1:2, function(x) postModProbs(x, rep(1/length(W), length(W))))
+  
+  # How often does the pf pick the "right" model?
+  rm.max = apply(rm.postModProbs, 1:2, function(x) which(x == max(x)))
+  prob.matchTruth = apply(rm.max, 2, function(x) sum(x == which(true.postModProbs == max(true.postModProbs)))/length(nsims))
+  
+  # Ternary diagrams of posterior model probabilities
+  require(compositions)
+  wlabels = rep(NA,length(W))
+  for(i in 1:length(W)) wlabels[i] = as.expression(bquote(paste(tilde(W)," = ",sep="") ~ .(W[i])))
+  pdf(file=paste(gpath,"cv_pf_ternary-",n.sim,".pdf",sep=""),width=10,height=10)
+  par(mfrow=c(2,2))
+  for(i in 1:length(np))
+  {
+    plot(acomp(rm.postModProbs[,i,]),labels=wlabels,lwd=2)
+    plot(acomp(true.postModProbs),add=TRUE,col=2,lwd=2)
+    mtext(paste(np[i]," particles",sep=""),side=3,cex=2)
+    if(i==1) legend("topleft",legend="True Posterior",pch=1,col=2,pt.lwd=2)
+  }
+  dev.off()
 }
 
-# Plot kernel density estimates of of log marginal likelihoods for pfs compared with truth
-require(plyr)
-max.densities = aaply(rm.lmarglik, 2:3, function(a) max(density(a)$y,na.rm=TRUE))
-min.densities = aaply(rm.lmarglik, 2:3, function(a) min(density(a)$y,na.rm=TRUE))
-max.breaks = aaply(rm.lmarglik, 2:3, function(a) max(density(a)$x,na.rm=TRUE))
-min.breaks = aaply(rm.lmarglik, 2:3, function(a) min(density(a)$x,na.rm=TRUE))
-ylabs = c("Density","","")
-xlabs = c("Log Marginal Likelihood","","")
-cols = rainbow(length(np))
-mains = c(substitute(paste(tilde(W)," = ",aa,sep=""),list(aa=W[1])),substitute(paste(tilde(W)," = ",aa,sep=""),list(aa=W[2])),substitute(paste(tilde(W)," = ",aa,sep=""),list(aa=W[3])))
-pdf(file=paste(gpath,"cv_pf_loglik.pdf",sep=""),width=15,height=5)
-par(mfrow=c(1,3),mar=c(5,6,4,2)+0.1)
-for(i in 1:3)
-{
-  plot(density(rm.lmarglik[,1,i]),lwd=2,col=cols[1],main=mains[i],xlab=xlabs[i],ylab=ylabs[i],xlim=c(min(min.breaks),max(max.breaks)),ylim=c(min(min.densities[,i]),max(max.densities[,i])),cex.axis=1.5,cex.lab=1.75,cex.main=2)
-  if(length(np > 1)) for(j in 2:length(np)) lines(density(rm.lmarglik[,j,i]),lwd=2,col=cols[j])
-  abline(v=true.lmarglik[i],lwd=1,col=1)
-  for(j in which(!(1:3 %in% i))) abline(v=true.lmarglik[j],lwd=1,col=1,lty=2)
-  mtext(round(true.lmarglik[i],2),side=1,at=true.lmarglik[i],cex=0.75)
-  if(i == 1) legend("topleft",c(paste(np," particles",sep=""),"Truth","Truth (other models)"),lty=c(rep(1,length(np)),1,2),lwd=c(rep(2,length(np)),1,1),col=c(cols,1,1),cex=1.5)
-}
-dev.off()
-
-# Compute true posterior model probabilities
-true.postModProbs = postModProbs(true.lmarglik, rep(1/length(W), length(W)))
-
-# Compute rm_pf approximate posterior model probabilities
-rm.postModProbs = aaply(rm.lmarglik, 1:2, function(x) postModProbs(x, rep(1/length(W), length(W))))
-
-# How often does the pf pick the "right" model?
-rm.max = apply(rm.postModProbs, 1:2, function(x) which(x == max(x)))
-prob.matchTruth = apply(rm.max, 2, function(x) sum(x == which(true.postModProbs == max(true.postModProbs)))/length(nsims))
-
-# Ternary diagrams of posterior model probabilities
-require(compositions)
-labels = rep(NA,length(W))
-for(i in 1:length(W)) labels[i] = as.expression(bquote(paste(tilde(W)," = ",sep="") ~ .(W[i])))
-pdf(file=paste(gpath,"cv_pf_ternary.pdf",sep=""),width=10,height=10)
-par(mfrow=c(2,2))
-for(i in 1:length(np))
-{
-  plot(acomp(rm.postModProbs[,i,]),labels=labels,lwd=2)
-  plot(acomp(true.postModProbs),add=TRUE,col=2,lwd=2)
-  mtext(paste(np[i]," particles",sep=""),side=3,cex=2)
-  if(i==1) legend("topleft",legend="True Posterior",pch=1,col=2,pt.lwd=2)
-}
-dev.off()
+m_ply(data.frame(n.sim = 1),cv_pf_loglik)
 
 ## Plots analyzing rm pf runs between simulations
 # Calculate true log marginal likelihoods under each model
+
 true_lmarglik = function(n.sim, W)
 {
+  F = mysims[[n.sim]]$true.params$F[1,1,1]
+  G = mysims[[n.sim]]$true.params$G[1,1]
+  V = mysims[[n.sim]]$true.params$V[1,1]
   post = cv.post(mysims[[n.sim]]$y, F, G, V, W, 1, 1, 0, 1)
   return(cv.lmarglik(mysims[[n.sim]]$y, post$f, post$Q, post$a, post$b))
 }
-true.lmarglik = maply(expand.grid(n.sim=1:nsims, W=W), true_lmarglik)
+true.lmarglik = maply(expand.grid(n.sim=nsims, W=W), true_lmarglik)
 
 # Load approximate log marginal likelihoods for particle filter runs
-rm.lmarglik = array(NA, c(nsims,length(np),length(W)))
+alpha = 0.05
+rm.lmarglik = array(NA, c(length(nsims),length(np),length(W)))
 for(j in 1:length(np))
 {
   for(k in 1:length(W))
   {
-    for(i in 1:nsims)
+    for(i in nsims)
     {
-      seed = mydata$seed[which(mydata$n.sim == i & mydata$np == np[j] & mydata$W == W[k])][1]
-      load(paste(dpath,"cv_pf-",i,"-",np[j],"-",W[k],"-",seed,"-",alpha,".rdata",sep=""))
+      label = mydata$label[which(mydata$n.sim == i & mydata$np == np[j] & mydata$W == W[k])][1]
+      load(paste(dpath,"cv_pf-",i,"-",np[j],"-",W[k],"-",label,"-",alpha,".rdata",sep=""))
       rm.lmarglik[i,j,k] = pf.out$lmarglik
     }
   }
@@ -225,22 +235,22 @@ for(i in 1:dim(rm.max)[2])
 
 # Plot ternary diagrams for posterior model probabilities
 require(compositions)
+N=length(nsims)
 grad = order(true.postModProbs[,2],decreasing=TRUE)
-labels = rep(NA,length(W))
-for(i in 1:length(W)) labels[i] = as.expression(bquote(paste(tilde(W)," = ",sep="") ~ .(W[i])))
+wlabels = rep(NA,length(W))
+for(i in 1:length(W)) wlabels[i] = as.expression(bquote(paste(tilde(W)," = ",sep="") ~ .(W[i])))
 pdf(file=paste(gpath,"cv_pf_ternary-bw.pdf",sep=""),width=10,height=10)
 par(mfrow=c(2,2))
 plot(acomp(true.postModProbs[grad,]),labels=labels,col=gray.colors(N),lwd=2)
 mtext("True Posterior",side=3,cex=2)
 for(i in 2:length(np))
 {
-  plot(acomp(rm.postModProbs[grad,i,]),labels=labels,col=gray.colors(N),lwd=2)
+  plot(acomp(rm.postModProbs[grad,i,]),labels=wlabels,col=gray.colors(N),lwd=2)
   mtext(paste(np[i]," particles",sep=""),side=3,cex=2)
 }
 dev.off()
 
 # Plot binary diagrams for posterior model probabilities
-N=nsims
 pdf(file=paste(gpath,"cv_pf_binary.pdf",sep=""),width=12,height=8)
 par(mfrow=c(2,length(np)),mar=c(5,6,4,1)+.1)
 ylabs=c(substitute(paste("P(",tilde(W)," = ",aa,") vs P(",tilde(W)," = ",ab,")",sep=""),list(aa=W[2],ab=W[1])),rep("",length(np)-1))

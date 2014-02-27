a0d8daa51c1a18cb312fa602f57939856eea0b5a
dsheinson <sheinson@pstat.ucsb.edu>
2014-02-27 14:37:07 -0800

    Fixed code to simulate from DLM

--- code/dlm_sim.r
+++ code/dlm_sim.r
@@ -9,15 +9,27 @@ dlm.sim <- function(nt, F, G, V, W, m0, C0, beta, U = 0)
   stopifnot((dim(C0)[1] == dim(C0)[2]) & (dim(G)[1] == dim(G)[2]) & (dim(V)[1] == dim(V)[2]) & (dim(W)[1] == dim(W)[2]) & (dim(G)[1] == dim(W)[1]) & (dim(G)[1] == dim(C0)[1]) & (dim(G)[1] == length(m0)))
   p = dim(W)[1]; q = dim(V)[1]
   
-  if(is.null(dim(F))) F = array(F,c(1,q,nt))
+  if(is.null(dim(F))) F = array(F,c(q,p,nt))
   if(is.matrix(F)) F = array(rep(F,nt),dim=c(dim(F)[1],dim(F)[2],nt)) else stopifnot(length(dim(F) == 3) & dim(F)[1] == q & dim(F)[2] == p & dim(F)[3] == nt)
   
-  if(missing(beta)) beta = rep(0,q)
-  if(is.null(dim(U))) U = array(U,c(1,q,nt))
-  if(is.matrix(U)) U = array(rep(U,nt),dim=c(dim(U)[1],dim(U)[2],nt)) else stopifnot(length(dim(U) == 3) & dim(U)[1] == q & dim(U)[2] == p & dim(U)[3] == nt)
+  if(missing(beta)) beta = rep(0,1)
+  d = length(beta)
+  if(is.null(dim(U))) U = array(U,c(q,d,nt))
+  if(is.matrix(U))
+  {
+    stopifnot(dim(U)[1] == q & dim(U)[2] == d)
+    U = array(U,dim=c(q,d,nt))
+  } else {
+    stopifnot(length(dim(U) == 3) & dim(U)[1] == q & dim(U)[2] == d & dim(U)[3] == nt)
+  }
   
   # Simulate data
-  chol.C0 = chol(C0); chol.V = chol(V); chol.W = chol(W)
+  try(chol.C0 = chol(C0),silent=TRUE)
+  if(class(chol.C0) == "try-error") chol.C0 = chol(C0,pivot=TRUE)
+  try(chol.V = chol(V),silent=TRUE)
+  if(class(chol.V) == "try-error") chol.V = chol(V,pivot=TRUE)
+  try(chol.W = chol(W),silent=TRUE)
+  if(class(chol.W) == "try-error") chol.W = chol(W,pivot=TRUE)
   x0 = chol.C0%*%rnorm(p,m0,1)
   x = matrix(NA, nr = p, nc = nt + 1)
   y = matrix(NA, nr = q, nc = nt)

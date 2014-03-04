ad27f5b01c93f05e81e7217a72b1c700cc18f116
dsheinson <sheinson@pstat.ucsb.edu>
2014-03-04 15:16:54 -0800

    Re-run rm pf runs on dlm rw model

--- code/cv_rm.r
+++ code/cv_rm.r
@@ -11,9 +11,8 @@ dpath = "../data/"
 load(paste(dpath,"rw_sim.rdata",sep=""))
 
 # Function to run pfs
-pf_lmarglik <- function(n.sim, np, W, label, alpha = 0.05, progress = FALSE)
+pf_lmarglik <- function(n.sim, n.run, W, np, alpha = 0.05, progress = FALSE)
 {
-  rnorm(1)
   a0 = b0 = 1
   mydlm = list(F = 1, G = 1, V = 1, W = W, m0 = 0, C0 = 1)
   dllik1 = function(y, x, theta) dllik(y, x, sigma2 = theta)
@@ -30,15 +29,13 @@ pf_lmarglik <- function(n.sim, np, W, label, alpha = 0.05, progress = FALSE)
   theta.quant = pf.quantile(1/out$theta, out$weight, function(x, param=1) x, c(alpha/2,1-alpha/2))
   
   pf.out = list(lmarglik=lmarglik, state.quant=state.quant, theta.quant=theta.quant)
-  cat(n.sim,np,W,label,'\n')
-  save(pf.out, file=paste(dpath,"cv_pf-",n.sim,"-",np,"-",W,"-",label,"-",alpha,".rdata",sep=""))
+  cat(n.sim, n.run, W, np,'\n')
+  save(pf.out, file=paste(dpath,"cv_pf-",paste(n.sim, n.run, W, np, alpha,sep="-"),".rdata",sep=""))
 }
 
 # Apply pf_lmarglik for many pf runs with different number of particles and values of W
-data1 = expand.grid(n.sim=rep(c(1,2,3),c(20,20,20)),np=10000, W=2, stringsAsFactors=FALSE)
-data1 = data.frame(data1, label = 1:dim(data1)[1])
-data2 = expand.grid(n.sim=4:20,np=10000, W=c(0.5,1,2), stringsAsFactors=FALSE)
-data2 = data.frame(data2, label = (dim(data1)[1] + 1:dim(data2)[1]))
+data1 = expand.grid(n.run=1:20, W=c(0.1,0.5,1,2,3), np=c(500,1000,5000,10000), n.sim=1:3, stringsAsFactors=FALSE)
+data2 = expand.grid(n.run=1, W=c(0.1,0.5,1,2,3), np=c(500,1000,5000,10000), n.sim=4:20, stringsAsFactors=FALSE)
 mydata = rbind(data1,data2)
 require(doMC)
 registerDoMC()

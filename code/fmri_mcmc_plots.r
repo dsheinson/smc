66519625f889f37d0f253b6d24eef59e754e42f6
dsheinson <sheinson@pstat.ucsb.edu>
2014-03-03 21:31:59 -0800

    code to create traceplots for mcmc for dynamic regression model

--- code/fmri_mcmc_plots.r
+++ code/fmri_mcmc_plots.r
@@ -40,4 +40,66 @@ fmri_mcmc_plot <- function(n.sim, mod, n.chains, x=1, beta=1, sigma2m=1, phi=1, 
       abline(h=mysims[[n.sim]]$true.params$beta[i])
     }
     dev.off()
+  }
+  
+  # Traceplots for sigma2m
+  if(sigma2m)
+  {
+    iter = (1:n.keep)*n.thin
+    pdf(file=paste(gpath,"fmri_mcmc_test-",paste(n.sim,mod,sep="-"),"-traceplots-sigma2m.pdf",sep=""))
+    par(mfrow=c(1,1), mar=c(5,6,4,2)+.1)
+    ymin = min(sapply(out.all, function(x) min(x$sigma2m)))
+    ymax = max(sapply(out.all, function(x) max(x$sigma2m)))
+    plot(iter,out.all[[1]]$sigma2m,type="l",ylim=c(ymin,ymax),xlab="Iteration",ylab=expression(sigma[m]^2),cex.lab=1.5)
+    if(n.chains > 1)
+    {
+      for(j in 2:n.chains) lines(iter,out.all[[j]]$sigma2m,col=2*(j-1))
+    }
+    abline(h=mysims[[n.sim]]$true.params$V[1,1])
+    dev.off()
+  }
+  
+  # Traceplots for phi
+  if(phi)
+  {
+    iter = (1:n.keep)*n.thin
+    p = length(mysims[[n.sim]]$true.params$G[,1])
+    pdf(file=paste(gpath,"fmri_mcmc_test-",paste(n.sim,mod,sep="-"),"-traceplots-phi.pdf",sep=""))
+    par(mfrow=c(p,1), mar=c(5,6,4,2)+.1)
+    mins = apply(matrix(sapply(out.all, function(x) apply(x$phi, 2, min)),nr=p), 1, min)
+    maxs = apply(matrix(sapply(out.all, function(x) apply(x$phi, 2, max)),nr=p), 1, max)
+    
+    for(i in 1:p)
+    {
+      ylab = bquote(expression(phi[.(i)]))
+      plot(iter,out.all[[1]]$phi[,i],type="l",ylim=c(mins[i],maxs[i]),xlab="Iteration",ylab=eval(ylab),cex.lab=1.5)
+      if(n.chains > 1)
+      {
+        for(j in 2:n.chains) lines(iter,out.all[[j]]$phi[,i],col=2*(j-1))
+      }
+      abline(h=mysims[[n.sim]]$true.params$G[i,1])
+    }
+    dev.off()
+  }
+  
+  # Traceplots for sigma2s
+  if(sigma2s)
+  {
+    iter = (1:n.keep)*n.thin
+    pdf(file=paste(gpath,"fmri_mcmc_test-",paste(n.sim,mod,sep="-"),"-traceplots-sigma2s.pdf",sep=""))
+    par(mfrow=c(1,1), mar=c(5,6,4,2)+.1)
+    ymin = min(sapply(out.all, function(x) min(x$sigma2s)))
+    ymax = max(sapply(out.all, function(x) max(x$sigma2s)))
+    plot(iter,out.all[[1]]$sigma2s,type="l",ylim=c(ymin,ymax),xlab="Iteration",ylab=expression(sigma[s]^2),cex.lab=1.5)
+    if(n.chains > 1)
+    {
+      for(j in 2:n.chains) lines(iter,out.all[[j]]$sigma2s,col=2*(j-1))
+    }
+    abline(h=mysims[[n.sim]]$true.params$W[1,1])
+    dev.off()
+  }
 }
+
+require(plyr)
+mydata = expand.grid(n.sim = 1, mod = "M101", n.chains = 3, x=1, beta=1, sigma2m=1, phi=1, sigma2s=1, stringsAsFactors=FALSE)
+m_ply(mydata, fmri_mcmc_plot)

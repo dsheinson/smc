909964268b1d057acb766064da866b64b19c8681
dsheinson <sheinson@pstat.ucsb.edu>
2014-05-10 16:45:49 -0700

    Updated code to create basis functions for fmri data analysis

--- code/fmri_design.r
+++ code/fmri_design.r
@@ -5,19 +5,19 @@ dpath = "../data/"
 gpath = "../graphs/"
 
 # Function to simulate design matrices and choose most stable
-optim.design <- function(N, n, E, intercept = TRUE)
+optim.design <- function(N, n, E, intercept = TRUE, ...)
 {
-  hrf = boynton.hrf(n)$hrf
+  hrf = boynton.hrf(n, ...)$hrf
   designs = list(); length(designs) = N
   for(j in 1:N)
   {
-    boxcar = rapidEvent.boxcar(n, E)
-    basis1 = fmri.convolve(hrf,boxcar$boxcar[,1])
+    boxcar = rapidEvent.boxcar(n, E, ...)
+    basis1 = fmri.convolve(hrf,boxcar$boxcar[,1], ...)
     if(E > 1)
     {
       basis = matrix(NA, nr = length(basis1$t), nc = E)
       basis[,1] = basis1$basis
-      for(i in 2:E) basis[,i] = fmri.convolve(hrf,boxcar$boxcar[,i])$basis
+      for(i in 2:E) basis[,i] = fmri.convolve(hrf,boxcar$boxcar[,i], ...)$basis
     } else {basis = as.matrix(basis1$basis)}
     if(intercept) X = cbind(1,basis) else X = basis
     eig = eigen(t(X)%*%X)
@@ -29,7 +29,7 @@ optim.design <- function(N, n, E, intercept = TRUE)
   ind = which(sapply(designs, function(x) x$min.eig) == max(sapply(designs, function(x) x$min.eig)))
   
   # Plot optimal design
-  pdf(file = paste(gpath,"fmri-design-",dim(designs[[ind]]$boxcar[[2]])[2],".pdf",sep=""))
+  pdf(file = paste(gpath,"fmri-design-",N,"-",n,"-",E,".pdf",sep=""))
   par(mfrow=c(3,1))
   ymax = max(designs[[ind]]$boxcar$boxcar)
   plot(designs[[ind]]$boxcar$t,designs[[ind]]$boxcar$boxcar[,1],ylim=c(0,ymax),type="l",xlab="",ylab="Boxcar")
@@ -44,5 +44,7 @@ optim.design <- function(N, n, E, intercept = TRUE)
 }
 
 set.seed(99)
-fmri.design = optim.design(10,500,1)
-save(fmri.design, file = paste(dpath,"fmri-design-",dim(fmri.design$boxcar$boxcar)[2],".rdata",sep=""))
+require(plyr)
+mydata = expand.grid(N = 1, n = c(1000,2000,10000,20000), E = 1, d = 1)
+fmri.design = mlply(mydata, optim.design)
+save(fmri.design, file = paste(dpath,"fmri-design.rdata",sep=""))

8db37bc638a91cce9a5cd7d71dc235b38d7c7864
dsheinson <sheinson@pstat.ucsb.edu>
2014-05-19 12:42:04 -0700

    Redo some mcmc sims, finish rm pf sims, save particles for some

--- code/fmri_rm_test.r
+++ code/fmri_rm_test.r
@@ -9,7 +9,7 @@ dpath = "../data/"
 gpath = "../graphs/"
 
 # Function to run pfs
-fmri_rm <- function(N, mod.sim, dimx, n, n.sim, n.run, mod.est, np, sd.fac = 1, alpha = 0.05, progress = TRUE)
+fmri_rm <- function(N, mod.sim, dimx, n, n.sim, n.run, mod.est, np, sd.fac = 1, alpha = 0.05, progress = TRUE, save = FALSE)
 {
    # Load simulated data
   load(paste(dpath,"dlm_ar_sim-",N,"-",mod.sim,"-",dimx,".rdata",sep=""))
@@ -43,7 +43,7 @@ fmri_rm <- function(N, mod.sim, dimx, n, n.sim, n.run, mod.est, np, sd.fac = 1, 
   state.quant = pf.quantile(out$state, out$weight, function(x, param=1) x, c(alpha/2,1-alpha/2))
   theta.quant = pf.quantile(out$theta, out$weight, function(x, param=1) x, c(alpha/2,1-alpha/2))
   
-  pf.out = list(lmarglik=lmarglik, state.quant=state.quant, theta.quant=theta.quant)
+  if(!save) pf.out = list(lmarglik=lmarglik, state.quant=state.quant, theta.quant=theta.quant) else pf.out = list(out=out, lmarglik=lmarglik, state.quant=state.quant, theta.quant=theta.quant)
   file = paste(dpath,"fmri_rm-",paste(N,mod.sim,dimx,n,n.sim,n.run,mod.est,np,sd.fac,100*alpha,sep="-"),".rdata",sep="")
   print(file)
   save(pf.out, file=file)
@@ -52,7 +52,11 @@ fmri_rm <- function(N, mod.sim, dimx, n, n.sim, n.run, mod.est, np, sd.fac = 1, 
 require(plyr)
 require(doMC)
 registerDoMC()
-data1 = expand.grid(N = 20, mod.est = c("M101","M011"), dimx = 2, n.sim = 1:8, n.run = 1:3, mod.sim = c("M101","M011"), sd.fac = 1, n = c(1,6,11,16), np = 100, progress = FALSE)
-data2 = expand.grid(N = 20, mod.est = c("M101","M011"), dimx = 2, n.sim = 1:8, n.run = 1:3, mod.sim = c("M101","M011"), sd.fac = c(3,5,7), n = 6, np = 100, progress = FALSE)
-mydata = rbind(data1,data2)
+# data1 = expand.grid(N = 20, mod.est = c("M101","M011"), dimx = 2, n.sim = 1:8, n.run = 1:3, mod.sim = c("M101","M011"), sd.fac = 1, n = c(1,6,11,16), np = 100, progress = FALSE)
+# data2 = expand.grid(N = 20, mod.est = c("M101","M011"), dimx = 2, n.sim = 1:8, n.run = 1:3, mod.sim = c("M101","M011"), sd.fac = c(3,5,7), n = 6, np = 100, progress = FALSE)
+data1 = expand.grid(N = 20, mod.est = "M011", dimx = 2, n.sim = c(1,3), n.run = 2:3, mod.sim = "M011", sd.fac = 1, n = 16, np = 100, progress = FALSE, save = FALSE, stringsAsFactors = FALSE)
+data2 = expand.grid(N = 20, mod.est = "M011", dimx = 2, n.sim = c(5,7), n.run = 1:3, mod.sim = "M011", sd.fac = 1, n = 16, np = 100, progress = FALSE, save = FALSE, stringsAsFactors = FALSE)
+data3 = expand.grid(N = 20, mod.est = "M011", dimx = 2, n.sim = c(1,3,5,7), n.run = 1:3, mod.sim = c("M101","M011"), sd.fac = c(5,7), n = 6, np = 100, progress = FALSE, save = FALSE, stringsAsFactors = FALSE)
+data4 = expand.grid(N = 20, mod.est = c("M101","M011"), dimx = 2, n.sim = 1:8, n.run = 1:3, mod.sim = c("M101","M011"), sd.fac = 1, n = 6, np = 100, progress = FALSE, save = TRUE, stringsAsFactors = FALSE)
+mydata = rbind(data1,data2,data3,data4)
 m_ply(.dat = mydata, .fun = fmri_rm, .parallel = TRUE)

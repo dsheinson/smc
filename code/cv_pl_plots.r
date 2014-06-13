d6eb7a86aa3e099caf1ca0ff5ce77b1b1d8dc408
dsheinson <sheinson@pstat.ucsb.edu>
2014-06-13 12:32:12 -0700

    Updated cv plots

--- code/cv_pl_plots.r
+++ code/cv_pl_plots.r
@@ -6,7 +6,7 @@ source("pf_mc_functions.r")
 dpath = "../data/"
 gpath = "../graphs/"
 
-cv_pl_quantiles <- function(lambda, np, nrtot, nruns, nsims, filt, alpha = 0.05, burn = 0)
+cv_pl_quantiles <- function(lambda, np, nrtot, nruns, nsims, filt, alpha = 0.05, burn = 0, ymin, ymax)
 {
   # Plot quantiles for each sim for each # particles
   for(n.sim in nsims)
@@ -14,6 +14,7 @@ cv_pl_quantiles <- function(lambda, np, nrtot, nruns, nsims, filt, alpha = 0.05,
     # Load simulated data
     load(paste(dpath,"rw_sim.rdata",sep=""))
     y = mysims[[n.sim]]$y
+    nt = dim(y)[2]
     
     # Calculate true 95% CI of states and precision
     post = cv.post(mysims[[n.sim]]$y, F=1, G=1, V=1, W=lambda, a0=1, b0=1, m0=0, C0=1)
@@ -22,6 +23,54 @@ cv_pl_quantiles <- function(lambda, np, nrtot, nruns, nsims, filt, alpha = 0.05,
     lp = qgamma(alpha/2,post$a,post$b)
     up = qgamma(1-alpha/2,post$a,post$b)
     
+    # Find ymax and ymin if missing
+    if(missing(ymin) | missing(ymax))
+    {
+      ymin = c(Inf, Inf)
+      ymax = c(-Inf, -Inf)
+      for(j in 1:length(np))
+      {
+        load(paste(dpath,"cv_pl-",lambda,"-",np[j],"-",nrtot,"-",n.sim,".rdata",sep=""))
+        attach(pf.out,warn.conflicts=F)
+        if(missing(filt))
+        {
+          filt = rownames(lmarglik)
+          filt.ind = 1:length(filt)
+        } else {
+          filt.ind = which(rownames(lmarglik) %in% filt)
+        }
+        for(k in filt.ind)
+        {
+          if(burn > 0)
+          {
+            g.lk = lk[-(1:burn)]
+            g.uk = uk[-(1:burn)]
+            g.ls = sapply(state.quant, function(x) x[[k]][-(1:burn),1,1])
+            g.us = sapply(state.quant, function(x) x[[k]][-(1:burn),1,2])
+            g.lp = lp[-(1:burn)]
+            g.up = up[-(1:burn)]
+            g.lf = sapply(theta.quant, function(x)  x[[k]][-(1:burn),1,1])
+            g.uf = sapply(theta.quant, function(x)  x[[k]][-(1:burn),1,2])     
+          } else {
+            g.lk = lk
+            g.uk = uk
+            g.ls = sapply(state.quant, function(x) x[[k]][,1,1])
+            g.us = sapply(state.quant, function(x) x[[k]][,1,2])
+            g.lp = lp
+            g.up = up
+            g.lf = sapply(theta.quant, function(x)  x[[k]][,1,1])
+            g.uf = sapply(theta.quant, function(x)  x[[k]][,1,2])
+          }
+          ymin[1] = min(ymin[1], mysims[[n.sim]]$x[1,], g.lk, apply(g.ls,2,min))
+          ymin[2] = min(ymin[2], 1, g.lp, apply(g.lf,2,min))
+          ymax[1] = max(ymax[1], mysims[[n.sim]]$x[1,], g.uk, apply(g.us,2,max))
+          ymax[2] = max(ymax[2], 1, g.up, apply(g.uf,2,max))
+        }
+      }
+    }
+    
+    pdf(paste(gpath,"cv-pl-quant-",10*lambda,"-",n.sim,"-",nrtot,"-",nruns,".pdf",sep=""),width=10,height=5*length(np))
+    par(mfrow=c(length(np),2), mar=c(5,6,4,2)+0.1)
     for(j in 1:length(np))
     {
       # Load data
@@ -35,77 +84,59 @@ cv_pl_quantiles <- function(lambda, np, nrtot, nruns, nsims, filt, alpha = 0.05,
         filt.ind = which(rownames(lmarglik) %in% filt)
       }
       
-      # Plot 95% CI for states and precision
-      pdf(paste(gpath,"cv-pl-quant-",10*lambda,"-",n.sim,"-",nrtot,"-",nruns,"-",np[j],".pdf",sep=""),width=10,height=5)
-      par(mfrow=c(1,2))
-      nt = dim(y)[2]
-      if(burn > 0)
+      # If only one rm in filt, delete lag from label
+      filt.names = filt
+      firsttwo = sapply(strsplit(filt,""), function(x) paste(x[1:2],sep="",collapse=""))
+      ind = which(firsttwo == 'rm')
+      if(length(ind) == 1) filt.names[ind] = 'rm'
+      
+      if(j == 1)
       {
-        g.lk = lk[-(1:burn)]
-        g.uk = uk[-(1:burn)]
-        g.ls = sapply(state.quant, function(x) sapply(x, function(w) w[-(1:burn),1,1]))
-        g.us = sapply(state.quant, function(x) sapply(x, function(w) w[-(1:burn),1,2]))
+        xlab = c(expression(t),"")
+        ylab = c(paste("J = ",np[j],sep=""),"")
+        main = paste("95% CI for Filtered ", c("States","Precision"), sep="") 
       } else {
-        g.lk = lk
-        g.uk = uk
-        g.ls = sapply(state.quant, function(x) sapply(x, function(w) w[,1,1]))
-        g.us = sapply(state.quant, function(x) sapply(x, function(w) w[,1,2]))
+        xlab = c("","")
+        ylab = c(paste("J = ",np[j],sep=""),"")
+        main = c("","")
       }
-      gmin = min(mysims[[n.sim]]$x[1,], g.lk, apply(g.ls,2,min))
-      gmax = max(mysims[[n.sim]]$x[1,], g.uk, apply(g.us,2,max))
-      plot(0:nt,mysims[[n.sim]]$x[1,],ylim=c(gmin,gmax),type="l",xlab=expression(t),ylab="Position",col='gray',lwd=3)
-      mtext(paste(np[j]," particles",sep=""),side=3)
-      for(i in filt.ind)
+      
+      # Plot 95% CI for states
+      plot(0:nt,mysims[[n.sim]]$x[1,],ylim=c(ymin[1],ymax[1]),type="l",main=main[1],xlab=xlab[1],ylab=ylab[1],col='gray',lwd=3, cex.lab = 2, cex.main = 2, cex.axis = 1.25)
+      for(i in 1:length(filt.ind))
       {
         for(k in 1:nruns)
         {
-          lines(0:nt,state.quant[[k]][[i]][,1,1],col=i+1)
-          lines(0:nt,state.quant[[k]][[i]][,1,2],col=i+1)
+          lines(0:nt,state.quant[[k]][[filt.ind[i]]][,1,1],col=i+1)
+          lines(0:nt,state.quant[[k]][[filt.ind[i]]][,1,2],col=i+1)
         }
       }
       lines(0:nt,mysims[[n.sim]]$x[1,],lwd=3,col="gray")
       lines(0:nt,lk,lwd=3)
       lines(0:nt,uk,lwd=3)
-      legend("topleft",legend=c(filt, "True Post", "True Sim"),lty=c(rep(1,length(filt)),1,1),col=c(2:(length(filt)+1),1,'gray'),lwd=c(rep(1,length(filt)),1,2))
-      title("95% CI for filtered states")
-      
+   
       # Plot 95% CI for precision
-      if(burn > 0)
-      {
-        g.lp = lp[-(1:burn)]
-        g.up = up[-(1:burn)]
-        g.lf = sapply(theta.quant, function(x)  sapply(x, function(w) w[-(1:burn),1,1]))
-        g.uf = sapply(theta.quant, function(x)  sapply(x, function(w) w[-(1:burn),1,2]))
-      } else {
-        g.lp = lp
-        g.up = up
-        g.lf = sapply(theta.quant, function(x)  sapply(x, function(w) w[,1,1]))
-        g.uf = sapply(theta.quant, function(x)  sapply(x, function(w) w[,1,2]))
-      }
-      gmin = min(1, g.lp, apply(g.lf,2,min))
-      gmax = max(1, g.up, apply(g.uf,2,max))
-      plot(0:nt,lp,type="l",lwd=3,ylim=c(gmin,gmax),main="95% CI for Filtered Precision",xlab=expression(t),ylab=expression(1/theta))
-      mtext(paste(np[j]," particles",sep=""),side=3)
-      for(i in filt.ind)
+      plot(0:nt,lp,type="l",lwd=3,ylim=c(ymin[2],ymax[2]),main=main[2],xlab=xlab[2],ylab=ylab[2], cex.lab = 2, cex.main = 2, cex.axis = 1.25)
+      for(i in 1:length(filt.ind))
       {
         for(k in 1:nruns)
         {
-          lines(0:nt,theta.quant[[k]][[i]][,1,1],col=i+1)
-          lines(0:nt,theta.quant[[k]][[i]][,1,2],col=i+1)
+          lines(0:nt,theta.quant[[k]][[filt.ind[i]]][,1,1],col=i+1)
+          lines(0:nt,theta.quant[[k]][[filt.ind[i]]][,1,2],col=i+1)
         }
       }
       lines(0:nt,lp,lwd=3)
       lines(0:nt,up,lwd=3)
       abline(h=1,col='gray',lwd=3)
-      legend("topright",legend=c(filt, "True Post", "True Sim"),lty=c(rep(1,length(filt)),1,1),col=c(2:(length(filt)+1),1,'gray'),lwd=c(rep(1,length(filt)),1,2))
-      dev.off()
+      if(j == 1) legend("topright",legend=c(filt.names, "True Post", "True Sim"),lty=c(rep(1,length(filt.names)),1,1),col=c(2:(length(filt.names)+1),1,'gray'),lwd=c(rep(1,length(filt.names)),3,3), cex=1.5)
     }
+    dev.off()
   }
 }
 
 require(plyr)
-mydata = expand.grid(lambda = c(.5,1,2), np=c(100), nrtot=20, nruns=10, stringsAsFactors=FALSE)
-m_ply(mydata, function(lambda, np, nrtot, nruns) cv_pl_quantiles(lambda, np, nrtot, nruns, 1, c('pl', 'kd', 'rm100')))
+mydata = expand.grid(lambda = c(.5,1,2), nrtot=20, nruns=5, stringsAsFactors=FALSE)
+m_ply(mydata, function(lambda, nrtot, nruns) cv_pl_quantiles(lambda, np = c(100,500,1000,5000), nrtot, nruns, 1, c('pl', 'kd', 'rm100'), burn = 5))
 
 cv_pl_loglik_wsim = function(np, nruns, nsims, lambda = c(.5, 1, 2), filt = c('pl','plrb','kd','rm'), blim, dmax)
 {
@@ -136,7 +167,12 @@ cv_pl_loglik_wsim = function(np, nruns, nsims, lambda = c(.5, 1, 2), filt = c('p
          lmargliks[[j]][,,k] = lmarglik[which(rownames(lmarglik) %in% filt),]
       }
     }
-
+    
+    # If only one rm in filt, delete lag from label
+    firsttwo = sapply(strsplit(filt,""), function(x) paste(x[1:2],sep="",collapse=""))
+    ind = which(firsttwo == 'rm')
+    if(length(ind) == 1) filt[ind] = 'rm'
+    
     # Plot kernel density estimates of of log marginal likelihoods for pfs compared with truth
     if(missing(dmax)) dmax = max(sapply(lmargliks, function(x) apply(x, c(1,3), function(a) max(density(a)$y,na.rm=TRUE))))
     if(missing(blim))
@@ -167,8 +203,10 @@ cv_pl_loglik_wsim = function(np, nruns, nsims, lambda = c(.5, 1, 2), filt = c('p
           xlab = ""
           ylab = paste("J = ",np[j],sep="")
           main = ""
+        } else {
+          xlab = ylab = main = ""
         }
-        plot(density(lmargliks[[j]][1,,i]),lwd=2,col=cols[1],main=main,xlab=xlab,ylab=ylab,xlim=c(bmin,bmax),ylim=c(0,dmax),cex.axis=1.5,cex.lab=1.75,cex.main=2)
+        plot(density(lmargliks[[j]][1,,i]),lwd=2,col=cols[1],main=main,xlab=xlab,ylab=ylab,xlim=c(bmin,bmax),ylim=c(0,dmax),cex.axis=1.5,cex.lab=2,cex.main=2.25)
         if(length(filt > 1)) for(k in 2:length(filt)) lines(density(lmargliks[[j]][k,,i]),lwd=2,col=cols[k])
         abline(v=true.lmarglik[i],lwd=1,col=1)
         for(l in which(!(1:length(lambda) %in% i))) abline(v=true.lmarglik[l],lwd=1,col=1,lty=2)
@@ -181,8 +219,8 @@ cv_pl_loglik_wsim = function(np, nruns, nsims, lambda = c(.5, 1, 2), filt = c('p
 }
 
 require(plyr)
-mydata = expand.grid(nruns = 2, nsims = 1)
-m_ply(mydata, function(nruns, nsims) cv_pl_loglik_wsim(c(100), nruns, nsims, filt = c('pl','kd','rm100','rm50','rm25')))
+mydata = expand.grid(nruns = 20, nsims = 1)
+m_ply(mydata, function(nruns, nsims) cv_pl_loglik_wsim(c(100,500,1000,5000), nruns, nsims, filt = c('pl','kd','rm100'), blim = c(-200,-190)))
 
 cv_pl_comp_wsim = function(np, nruns, nsims, lambda = c(.5, 1, 2), filt = c('pl','plrb','kd','rm'))
 {
@@ -216,6 +254,11 @@ cv_pl_comp_wsim = function(np, nruns, nsims, lambda = c(.5, 1, 2), filt = c('pl'
       }
     }
     
+    # If only one rm in filt, delete lag from label
+    firsttwo = sapply(strsplit(filt,""), function(x) paste(x[1:2],sep="",collapse=""))
+    ind = which(firsttwo == 'rm')
+    if(length(ind) == 1) filt[ind] = 'rm'
+    
     # Ternary diagrams of posterior model probabilities
     # Compute true posterior model probabilities
     true.postModProbs = postModProbs(true.lmarglik, rep(1/length(lambda), length(lambda)))
@@ -236,16 +279,16 @@ cv_pl_comp_wsim = function(np, nruns, nsims, lambda = c(.5, 1, 2), filt = c('pl'
     par(mfrow=c(2,2))
     for(i in 1:length(np))
     {
-      plot(acomp(pmargliks[[i]][,,1]),labels=wlabels, lwd=2)
-      if(length(filt) > 1) for(j in 2:length(filt)) plot(acomp(pmargliks[[i]][,,j]),add=TRUE, col = j,lwd=2)
+      plot(acomp(pmargliks[[i]][,,1]),labels=wlabels, col=2, lwd=2)
+      if(length(filt) > 1) for(j in 2:length(filt)) plot(acomp(pmargliks[[i]][,,j]),add=TRUE, col = j+1,lwd=2)
       plot(acomp(true.postModProbs),add=TRUE,col='gray',lwd=3)
       mtext(paste(np[i]," particles",sep=""),side=3,cex=2)
-      if(i==1) legend("topleft",legend=c(filt,"True Posterior"),pch=rep(1,length(filt)+1),col=c(1:length(filt),'gray'),pt.lwd=c(rep(2,length(filt)),3))
+      if(i==1) legend("topleft",legend=c(filt,"True Posterior"),pch=rep(1,length(filt)+1),col=c(2:(length(filt)+1),'gray'),pt.lwd=c(rep(2,length(filt)),3))
     }
     dev.off()
   }
 }
 
 require(plyr)
-mydata = expand.grid(nruns = 2, nsims = 1)
-m_ply(mydata, function(nruns, nsims) cv_pl_comp_wsim(c(100,500,1000), nruns, nsims, filt = c('pl','kd','rm100','rm50','rm25')))
+mydata = expand.grid(nruns = 20, nsims = 1)
+m_ply(mydata, function(nruns, nsims) cv_pl_comp_wsim(c(100,500,1000,5000), nruns, nsims, filt = c('pl','kd','rm100')))
